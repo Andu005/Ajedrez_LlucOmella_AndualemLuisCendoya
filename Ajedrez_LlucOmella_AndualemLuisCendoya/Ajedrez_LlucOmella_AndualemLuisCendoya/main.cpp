0e8f5a4aad61bc98b4c70c5870163c9a187bb763
Andu005 <andualem.cendoya@enti.cat>
2025-06-15 22:23:01 +0200

    Delete Ajedrez_LlucOmella_AndualemLuisCendoya directory

--- Ajedrez_LlucOmella_AndualemLuisCendoya/Ajedrez_LlucOmella_AndualemLuisCendoya/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "logica.h"
-#include <iostream>
-
-int main() { //funcio principal del projecte
-    loadRotors();
-    showMenu(); //main crida la resta de funcions per executar l'encriptador
-    return 0;
-}
