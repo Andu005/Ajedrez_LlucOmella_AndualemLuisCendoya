f6ea94d2822d6df6c07171b95a7650a19fb2b013
Andu005 <andualem.cendoya@enti.cat>
2025-06-15 22:42:31 +0200

    Add files via upload

---
+++ Ajedrez_LlucOmella_AndualemLuisCendoya/Ajedrez_LlucOmella_AndualemLuisCendoya/variables.cpp
@@ -0,0 +1,5 @@
+#include "logica.h"
+
+Rotor rotor1{ "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 'Z', 0 };
+Rotor rotor2{ "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 'Z', 0 };
+Rotor rotor3{ "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 'Z', 0 };
