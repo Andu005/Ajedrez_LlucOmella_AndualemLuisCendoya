c6ac4ef64597a4c3e64a9068199bfeda4a884925
Andu005 <andualem.cendoya@enti.cat>
2025-06-15 22:41:59 +0200

    Delete Ajedrez_LlucOmella_AndualemLuisCendoya directory

--- Ajedrez_LlucOmella_AndualemLuisCendoya/Ajedrez_LlucOmella_AndualemLuisCendoya/variables.cpp
+++
@@ -1,5 +0,0 @@
-#include "logica.h"
-
-Rotor rotor1{ "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 'Z', 0 };
-Rotor rotor2{ "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 'Z', 0 };
-Rotor rotor3{ "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 'Z', 0 };
