c0bd1412c99910263f011a7c46fd223d3b15d389
Andu005 <andualem.cendoya@enti.cat>
2025-06-15 11:17:16 +0200

    Comentari a const.h

--- Ajedrez_LlucOmella_AndualemLuisCendoya/Ajedrez_LlucOmella_AndualemLuisCendoya/const.h
+++ Ajedrez_LlucOmella_AndualemLuisCendoya/Ajedrez_LlucOmella_AndualemLuisCendoya/const.h
@@ -3,6 +3,7 @@
 
 #include <string>
 
+//declaració dels elements constants
 const std::string ROTOR1_FILE = "Rotor1.txt";
 const std::string ROTOR2_FILE = "Rotor2.txt";
 const std::string ROTOR3_FILE = "Rotor3.txt";
